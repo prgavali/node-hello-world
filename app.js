ff5c8084861ec1a07840b07a8889b41d11521a34
Pramod V. Gavali <prgavali@in.ibm.com>
2023-04-11 16:35:05 +0530

    added metrics support

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express')
+const lib = require("./lib/metrics");
 const app = express();
 
 const port = 8080;
@@ -9,6 +10,6 @@ const host = '0.0.0.0';
 app.get('/', (req, res) => {
   res.send('V2***Hello World from IBM Cloud Essentials!');
 })
-
+lib.exportMetrics(app)
 app.listen(port, host);
 console.log(`Running on http://${host}:${port}`);
