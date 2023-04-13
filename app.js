62c6021abf4f649cc8e32112032a5fe72d275f13
Pramod V. Gavali <prgavali@in.ibm.com>
2023-04-13 12:39:31 +0530

    demo up

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
   console.log("accessed /")
-  res.send('Hello World from IBM Cloud Essentials!');
+  res.send('Hello World from IBM Cloud Essentials!-V1');
 })
 lib.exportMetrics(app)
 app.listen(port, host);
